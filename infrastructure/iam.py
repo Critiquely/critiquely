025c917daf77af3af8ad41099adf34964a9b9c72
Billy Michael <billy.michael@globallogic.com>
2025-05-27 08:16:45 +0100

    chore(iac): Fixes to enable web identity for github actions

--- infrastructure/iam.py
+++ infrastructure/iam.py
@@ -4,14 +4,16 @@ import pulumi_aws as aws
 # Configuration
 account_id = "443170140241"
 provider_url = "https://token.actions.githubusercontent.com"
+thumbprint = "6938fd4d98bab03faadb97b34396831e3780aea1"
 audience = "sts.amazonaws.com"
-repo_filter = "repo:critiquely/*"
+repo_filter = "repo:Critiquely/*"
 
 # Create GitHub OIDC provider
 github_oidc_provider = aws.iam.OpenIdConnectProvider(
     "github_oidc_provider",
     url=provider_url,
-    client_id_lists=[audience]
+    client_id_lists=[audience],
+    thumbprint_lists=[thumbprint]
 )
 
 # Define trust policy document for GitHub Actions
@@ -25,7 +27,7 @@ assume_role_policy = aws.iam.get_policy_document(statements=[
         "actions": ["sts:AssumeRoleWithWebIdentity"],
         "conditions": [
             {
-                "test": "StringEquals",
+                "test": "StringLike",
                 "variable": "token.actions.githubusercontent.com:sub",
                 "values": [repo_filter],
             },
@@ -42,5 +44,8 @@ assume_role_policy = aws.iam.get_policy_document(statements=[
 github_oidc_role = aws.iam.Role(
     "github_oidc_role",
     name="github_oidc",
-    assume_role_policy=assume_role_policy.json
+    assume_role_policy=assume_role_policy.json,
+    managed_policy_arns=[
+        "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+    ]
 )
