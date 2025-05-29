f930f6766339c4b85ede1ffd73091aad1d6f5ae3
Billy Michael <billy.michael@globallogic.com>
2025-05-29 08:02:46 +0100

    chore(iac): renaming

--- infrastructure/lambda_stack.py
+++ infrastructure/lambda_stack.py
@@ -5,7 +5,7 @@ import pulumi_aws_apigateway as apigateway
 from ecr import ecr
 
 # An execution role to use for the Lambda function
-role = aws.iam.Role("role", 
+role = aws.iam.Role("webhook_fn_role", 
     assume_role_policy=json.dumps({
         "Version": "2012-10-17",
         "Statement": [{
@@ -19,14 +19,14 @@ role = aws.iam.Role("role",
     managed_policy_arns=[aws.iam.ManagedPolicy.AWS_LAMBDA_BASIC_EXECUTION_ROLE])
 
 # A Lambda function to invoke
-fn = aws.lambda_.Function("fn",
+fn = aws.lambda_.Function("webhook_fn",
     runtime="python3.9",
     handler="handler.handler",
     role=role.arn,
     image_uri=ecr.repository_url)
 
 # A REST API to route requests to HTML content and the Lambda function
-api = apigateway.RestAPI("api",
+api = apigateway.RestAPI("webhook_endpoint",
   routes=[
     apigateway.RouteArgs(path="/webhook", method=apigateway.Method.POST, event_handler=fn)
   ])
