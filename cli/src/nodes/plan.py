631d104c88569fdd250e969c2463979f16c9f741
Billy Michael <billy.michael@globallogic.com>
2025-06-05 21:16:11 +0100

    chore: add langraph example

--- cli/src/nodes/plan.py
+++
@@ -1,9 +0,0 @@
-import os
-from langchain.chat_models import init_chat_model
-
-os.environ["ANTHROPIC_API_KEY"] = "sk-..."
-
-llm = init_chat_model("anthropic:claude-3-5-sonnet-latest")
-
-def plan(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
